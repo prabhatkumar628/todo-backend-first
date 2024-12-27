b982db62ac363083f6afe6adfe356b4a49e72fba
Prabha Kumar <kprabhat628@gmail.com>
2024-12-27 17:34:57 +0530

    frontend url change

--- index.js
+++ index.js
@@ -34,14 +34,9 @@ connectDB()
     console.log("ERROR",error)
 })
 
-const allowedOrigins = [
-    process.env.FRONTEND_URL,
-    "http://localhost:5173"
-
-]
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials:true,
     methods:"GET,POST,PUT,DELETE",
     allowedHeaders:["Content-Type", "Authorization"]
